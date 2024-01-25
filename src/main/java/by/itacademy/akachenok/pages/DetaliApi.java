4338dc5405205eacd81f0f26a4e79cb3d673423e
sakachenok <akachenoksveta@gmail.com>
2024-01-25 11:15:55 +0300

    api tests were refactored. New method requestHeader was added into DetaliApi

--- src/main/java/by/itacademy/akachenok/pages/DetaliApi.java
+++ src/main/java/by/itacademy/akachenok/pages/DetaliApi.java
@@ -1,11 +1,21 @@
 package by.itacademy.akachenok.pages;
 
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
 public class DetaliApi {
     public static String bodyLoginWithUsernameWrongCredentials = "{\"login\":\"test@test.com\",\"password\":\"test\"}";
     public static String bodyLoginWithUsernameEmptyFields = "{\"login\":\"\",\"password\":\"\"}";
     public static String bodyLoginWithEmailWrongCredentials = "{\"email\":\"test@test.com\",\"password\":\"123\"}";
     public static String bodyLoginWithPhoneWrongCredentials = "{\"phone\":\"+375(22)222-22-22\",\"password\":\"1234556\"}";
     public static String url = "https://detali.by/api/v2/customer/login/";
+
+    public static RequestSpecification requestHeader() {
+        RequestSpecification header = given()
+                .contentType("text/plain;charset=UTF-8");
+                return header;
+    }
 
 
 }
--- src/test/java/by/itacademy/akachenok/api/DetaliApiTest.java
+++ src/test/java/by/itacademy/akachenok/api/DetaliApiTest.java
@@ -3,41 +3,44 @@ package by.itacademy.akachenok.api;
 import by.itacademy.akachenok.pages.DetaliApi;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class DetaliApiTest {
 
     @Test
     public void testLoginWithUsernameWrongCredentials() {
-        given().header("Content-Type", "text/plain;charset=UTF-8")
+        DetaliApi.requestHeader()
                 .body(DetaliApi.bodyLoginWithUsernameWrongCredentials)
                 .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Логин или пароль введены неверно"));
+                .then().statusCode(401).body("errorMessage", equalTo("Логин или пароль введены неверно"))
+                .log().all();
     }
 
     @Test
     public void testLoginWithUsernameEmptyFields() {
-        given().header("Content-Type", "text/plain;charset=UTF-8")
+        DetaliApi.requestHeader()
                 .body(DetaliApi.bodyLoginWithUsernameEmptyFields)
                 .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами логин в базе данных не найден."));
+                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами логин в базе данных не найден."))
+                .log().all();
     }
 
     @Test
     public void testLoginWithEmailWrongCredentials() {
-        given().header("Content-Type", "text/plain;charset=UTF-8")
+        DetaliApi.requestHeader()
                 .body(DetaliApi.bodyLoginWithEmailWrongCredentials)
                 .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами email в базе данных не найден."));
+                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами email в базе данных не найден."))
+                .log().all();
     }
 
     @Test
     public void testLoginWithPhoneWrongCredentials() {
-        given().header("Content-Type", "text/plain;charset=UTF-8")
+        DetaliApi.requestHeader()
                 .body(DetaliApi.bodyLoginWithPhoneWrongCredentials)
                 .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами телефон в базе данных не найден."));
+                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами телефон в базе данных не найден."))
+                .log().all();
     }
 
 }
