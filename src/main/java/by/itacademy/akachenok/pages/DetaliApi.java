d31f84261eff0eafab08e89cfd815d7ff7a11733
sakachenok <akachenoksveta@gmail.com>
2024-01-28 01:01:30 +0300

    refactoring. Logger added

--- src/main/java/by/itacademy/akachenok/pages/DetaliApi.java
+++ src/main/java/by/itacademy/akachenok/pages/DetaliApi.java
@@ -5,16 +5,16 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
 
 public class DetaliApi {
-    public static String bodyLoginWithUsernameWrongCredentials = "{\"login\":\"test@test.com\",\"password\":\"test\"}";
-    public static String bodyLoginWithUsernameEmptyFields = "{\"login\":\"\",\"password\":\"\"}";
-    public static String bodyLoginWithEmailWrongCredentials = "{\"email\":\"test@test.com\",\"password\":\"123\"}";
-    public static String bodyLoginWithPhoneWrongCredentials = "{\"phone\":\"+375(22)222-22-22\",\"password\":\"1234556\"}";
-    public static String url = "https://detali.by/api/v2/customer/login/";
+    public static final String BODY_LOGIN_WITH_USERNAME_WRONG_CREDENTIALS = "{\"login\":\"test@test.com\",\"password\":\"test\"}";
+    public static final String BODY_LOGIN_WITH_USERNAME_EMPTY_FIELDS = "{\"login\":\"\",\"password\":\"\"}";
+    public static final String BODY_LOGIN_WITH_EMAIL_WRONG_CREDENTIALS = "{\"email\":\"test@test.com\",\"password\":\"123\"}";
+    public static final String BODY_LOGIN_WITH_PHONE_WRONG_CREDENTIALS = "{\"phone\":\"+375(22)222-22-22\",\"password\":\"1234556\"}";
+    public static final String URL = "https://detali.by/api/v2/customer/login/";
 
-    public static RequestSpecification requestHeader() {
+    public static RequestSpecification getHeaderContentTypeTextPlainUTF8() {
         RequestSpecification header = given()
                 .contentType("text/plain;charset=UTF-8");
-                return header;
+        return header;
     }
 
 }
--- src/test/java/by/itacademy/akachenok/api/DetaliApiTest.java
+++ src/test/java/by/itacademy/akachenok/api/DetaliApiTest.java
@@ -1,46 +1,53 @@
 package by.itacademy.akachenok.api;
 
 import by.itacademy.akachenok.pages.DetaliApi;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class DetaliApiTest {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Test
     public void testLoginWithUsernameWrongCredentials() {
-        DetaliApi.requestHeader()
-                .body(DetaliApi.bodyLoginWithUsernameWrongCredentials)
-                .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Логин или пароль введены неверно"))
-                .log().all();
+        LOGGER.info("testLoginWithUsernameWrongCredentials started");
+        DetaliApi.getHeaderContentTypeTextPlainUTF8()
+                .body(DetaliApi.BODY_LOGIN_WITH_USERNAME_WRONG_CREDENTIALS)
+                .when().post(DetaliApi.URL)
+                .then().statusCode(401).body("errorMessage", equalTo("Логин или пароль введены неверно"));
+        LOGGER.info("testLoginWithUsernameWrongCredentials passed");
     }
 
     @Test
     public void testLoginWithUsernameEmptyFields() {
-        DetaliApi.requestHeader()
-                .body(DetaliApi.bodyLoginWithUsernameEmptyFields)
-                .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами логин в базе данных не найден."))
-                .log().all();
+        LOGGER.info("testLoginWithUsernameEmptyFields started");
+        DetaliApi.getHeaderContentTypeTextPlainUTF8()
+                .body(DetaliApi.BODY_LOGIN_WITH_USERNAME_EMPTY_FIELDS)
+                .when().post(DetaliApi.URL)
+                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами логин в базе данных не найден."));
+        LOGGER.info("testLoginWithUsernameEmptyFields passed");
     }
 
     @Test
     public void testLoginWithEmailWrongCredentials() {
-        DetaliApi.requestHeader()
-                .body(DetaliApi.bodyLoginWithEmailWrongCredentials)
-                .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами email в базе данных не найден."))
-                .log().all();
+        LOGGER.info("testLoginWithEmailWrongCredentials started");
+        DetaliApi.getHeaderContentTypeTextPlainUTF8()
+                .body(DetaliApi.BODY_LOGIN_WITH_EMAIL_WRONG_CREDENTIALS)
+                .when().post(DetaliApi.URL)
+                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами email в базе данных не найден."));
+        LOGGER.info("testLoginWithEmailWrongCredentials passed");
     }
 
     @Test
     public void testLoginWithPhoneWrongCredentials() {
-        DetaliApi.requestHeader()
-                .body(DetaliApi.bodyLoginWithPhoneWrongCredentials)
-                .when().post(DetaliApi.url)
-                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами телефон в базе данных не найден."))
-                .log().all();
+        LOGGER.info("testLoginWithPhoneWrongCredentials started");
+        DetaliApi.getHeaderContentTypeTextPlainUTF8()
+                .body(DetaliApi.BODY_LOGIN_WITH_PHONE_WRONG_CREDENTIALS)
+                .when().post(DetaliApi.URL)
+                .then().statusCode(401).body("errorMessage", equalTo("Указанный Вами телефон в базе данных не найден."));
+        LOGGER.info("testLoginWithPhoneWrongCredentials passed");
     }
 
 }
